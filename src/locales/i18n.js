f6c8dd1928c4926d1af8fdbeed4517c979daec1f
Luis Murrugarra <luismurrugarra17@gmail.com>
2023-10-02 23:11:54 -0500

    build: new version I18n

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,12 +1,15 @@
 import {createI18n} from "vue-i18n";
 import en from "./en.json";
 import es from "./es.json";
+
 const i18n = createI18n({
-    locale:"es",
-    messages:{
-	en,
-	es
-    },
+  legacy: false, // Usa la nueva API de Vue I18n
+  locale: 'es', // Establece el idioma predeterminado
+  fallbackLocale: 'es', // Establece el idioma de respaldo
+  messages: {
+    en,
+    es
+  },
 
 });
 
